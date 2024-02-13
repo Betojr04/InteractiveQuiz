aedeb1505a729de81b0594d9a2543ef3707c47bf
Betojr04 <betomasia12@gmail.com>
2024-02-13 13:59:42 -0700

    STARTED STLYING NAVBAR finished added logo placeholder, search input, and links to other future pages of the website started stlying basic, the search input and adding padding and margins where applicable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+/* This component will be the navbar that will be displayed on the top of the page. It will include the logo, a search bar, and the links to the different pages of the blog. It will be a functional component. */
+
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
@@ -6,16 +8,31 @@ export const Navbar = () => {
   return (
     <nav>
       <div className="nav-logo">Logo</div>
-      <div className="nav-search">search bar</div>
+      {/* this form below will include a onSubmit={} once I start implementing the logic code. This comment serves as a reminder for future beto that this part still needs to be done */}
+      <form className="nav-search" action="/search" method="GET">
+        <label htmlFor="search">Search:</label>
+        <input type="search" name="query" />
+        <button type="submit">Search</button>{" "}
+        {/* this button will be hidden and will be used to submit the form with the search input */}
+      </form>
       <div className="nav-items">
         <li>
           <Link to="/">Blog</Link>
         </li>
         <li>
-          <Link to="/">About</Link>
+          <Link to="/">Recipes</Link>
         </li>
         <li>
-          <Link to="/">Features</Link>
+          <Link to="/">Key Features</Link>
+        </li>
+        <li>
+          <Link to="/">Our Team</Link>
+        </li>
+        <li>
+          <Link to="/">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/">Contact Us</Link>
         </li>
       </div>
     </nav>
