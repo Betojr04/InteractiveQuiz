0e3a4c94d3bf5c6673c6d9c413a5cc65bdb7ec60
Betojr04 <betomasia12@gmail.com>
2024-02-19 18:15:00 -0700

    messing around with different images as the hero section background/attraction

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ export const HeroSection = () => {
       <h1>PROSPERUM</h1>
       <p className="title-paragraph">Revolutionize Your Nutrition Experience</p>
       <img
-        src="https://images.unsplash.com/photo-1607522154118-75801cf8f2e9?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="https://images.unsplash.com/photo-1601972599720-36938d4ecd31?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="healthy"
       />
     </section>
