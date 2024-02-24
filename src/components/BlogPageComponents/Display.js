27cd16fa4bbdbb9f23aecb53a5f3c9c673b8d3b1
Betojr04 <betomasia12@gmail.com>
2024-02-23 23:22:28 -0700

    added blog page and created display component for the blog page that will hold all of the blog posts and allow you to click into a specific blog post and redirect to that blog post

---
+++ src/components/BlogPageComponents/Display.js
@@ -0,0 +1,13 @@
+/* 
+This is the display page of the blog. It will include the main content of the blog, which will be the posts. It will be a functional component. From here you will then be able to click on a specific post and be redirected to the post page.
+*/
+import React from "react";
+
+export const Display = () => {
+  return (
+    <section>
+      this is the lay out of the blog and will be where all the blogs are
+      displayed for you to click into a specific one.
+    </section>
+  );
+};
---
+++ src/views/ Blog.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Blog = () => {
+  return <main>This is the main page for the blog </main>;
+};
