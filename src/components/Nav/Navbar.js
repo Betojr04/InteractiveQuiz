6f7b505097923ca0884310c310fd538d99a2ff03
Betojr04 <betomasia12@gmail.com>
2024-02-28 10:03:29 -0700

    added in the nav items started addind in the useState for the hamburger menu

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -6,6 +6,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
+  const [hamburgerMenu, setHamburgerMenu] = useState();
+
   const searchButton = () => {
     alert("This button will be used to submit the form with the search input");
   };
@@ -18,36 +20,35 @@ export const Navbar = () => {
       </div>
       {/* this form below will include a onSubmit={} once I start implementing the logic code. This comment serves as a reminder for future beto that this part still needs to be done */}
       <form className="nav-search" action="/search" method="GET">
-        <label htmlFor="search">Search:</label>
         <input type="search" name="query" />
         <button onClick={searchButton} type="submit">
           Search
         </button>
       </form>
 
-      <div className="nav-items">
-        <ul>
-          <li>
-            <Link to="/">Blog</Link>
-          </li>
+      {/* <div className="nav-items"> */}
+      <ul className="nav-items">
+        <li>
+          <Link to="/">Blog</Link>
+        </li>
 
-          <li>
-            <Link to="/">Recipes</Link>
-          </li>
+        <li>
+          <Link to="/">Recipes</Link>
+        </li>
 
-          <li>
-            <Link to="/">Key Features</Link>
-          </li>
+        <li>
+          <Link to="/">Key Features</Link>
+        </li>
 
-          <li>
-            <Link to="/">Our Team</Link>
-          </li>
+        <li>
+          <Link to="/">Our Team</Link>
+        </li>
 
-          <li>
-            <Link to="/contactus">Contact Us/Sign Up</Link>
-          </li>
-        </ul>
-      </div>
+        <li>
+          <Link to="/contactus">Contact Us/Sign Up</Link>
+        </li>
+      </ul>
+      {/* </div> */}
     </nav>
   );
 };
