6a65d5a3cf80a651af060895fdf3fd24582b3a2c
Betojr04 <betomasia12@gmail.com>
2024-02-29 23:33:23 -0700

    starting styling and adding content to the features section have to figure out how to maintain the title centered but then have the three features taking up a 3rd of the width each

--- src/components/LandingPageComponents/FeaturesSection.js
+++ src/components/LandingPageComponents/FeaturesSection.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "../../styles/LandingPageStyles/featuressection.css";
 
 export const FeatureSection = () => {
   return (
     <section>
-      <div className="feature1"></div>
-      <div className="feature2"></div>
-      <div className="feature3"></div>
+      <h1>FEATURES</h1>
+      <div className="features-container">
+        <div className="feature1">Notification</div>
+        <div className="feature2"></div>
+        <div className="feature3"></div>
+      </div>
     </section>
   );
 };
