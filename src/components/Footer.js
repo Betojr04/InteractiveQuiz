52b920806eb840af5774751700e90abe8aa9e0fa
Betojr04 <betomasia12@gmail.com>
2024-03-04 21:51:00 -0700

    added links to the navbar items in the footer added instagram and linkedin logos the footer as well styled everything to be centered and to have 3 then 2 then the copyright info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,36 @@
 import React from "react";
 import "../styles/Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => {
   return (
     <footer>
+      <div className="footer-links">
+        <a href="/link1">Blog</a>
+        <a href="/link2">Our Team</a>
+        <a href="/link3">Features</a>
+      </div>
+      <div className="footer-links">
+        <a href="/link4">Recipes</a>
+        <a href="/link5">Contact/Sign Up</a>
+      </div>
+      <div className="social-icons">
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a
+          href="https://linkedin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </div>
       <p>© 2023 Prosperum</p>
     </footer>
   );
